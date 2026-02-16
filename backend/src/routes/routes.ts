18e4f0aefc1badbdd9ef7df0ab00d36314f83911
AyushhNK <neupaneayush3@gmail.com>
2026-02-16 18:21:30 +0545

    feat(route): routes handled for all modules

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,12 +2,20 @@ import { de } from "zod/v4/locales";
 import authRoutes from "../modules/auth/auth.routes";
 import userRoutes from "../modules/users/user.routes";
 import { Router } from "express";
+import notificationRoutes from "../modules/notification/notification.routes";
+import trackerRoutes from "../modules/tracker/tracker.routes";
+import goalRoutes from "../modules/goals/goals.routes";
+import progressRoutes from "../modules/progress/progress.routes";
 
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/notifications", notificationRoutes);
+router.use("/tracker", trackerRoutes);
+router.use("/goals", goalRoutes);
+router.use("/progress", progressRoutes);
 
 
 export default router;
